d0b5c90b9940f669aab0db170f7c38e9ca8cf820
Brad Bitler <bradbitler@berkeley.edu>
2024-06-25 09:51:05 -0700

    padding for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import { hero } from "@/lib/data";
 export default function Header() {
     
 	return (
-		<div className="w-screen box-border mx-auto">
-			<section className="rounded-3xl flex py-12 md:py-24 lg:py-32 xl:py-48  mx-6 bg-black">
+		<div className="w-screen box-border mx-auto mt-6">
+			<section className="rounded-3xl flex py- md:py-24 lg:py-32 xl:py-48  mx-6 bg-black">
 				<div className="flex flex-col justify-end p-6 text-white ">
 					<h1 className="lg:leading-tighter text-xl font-bold tracking-tighter sm:text-xl md:text-xl xl:text-5xl 2xl:text-4xl">
                     {hero.title}
